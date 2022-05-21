c133f829ace4e83f543bc1ecd130e0bbe5974521
Jesse Gibson <JesseTheGibson@gmail.com>
2022-05-21 15:13:55 -0700

    Generate type definitions with artifacts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import * as path from 'path';
 import { builtinModules } from 'module';
+import dts from 'vite-dts';
 
 export default defineConfig({
+  plugins: [dts()],
   build: {
     lib: {
       entry: path.join(__dirname, './src/index.ts'),
