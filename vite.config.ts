7b73bc596faf68b4b756c6e78936c844e849a40e
Jesse Gibson <JesseTheGibson@gmail.com>
2023-01-14 19:33:56 -0500

    Remove vite-dts
    It's unmaintained and I've found better ways to package libraries.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite';
 import * as path from 'path';
 import { builtinModules } from 'module';
-import dts from 'vite-dts';
 
 export default defineConfig({
-  plugins: [dts()],
   build: {
     lib: {
       entry: path.join(__dirname, './src/index.ts'),
       name: 'wardens',
-      fileName: (format: string) => `wardens.${format}.js`,
+      fileName: 'wardens',
     },
     rollupOptions: {
       external: builtinModules,
