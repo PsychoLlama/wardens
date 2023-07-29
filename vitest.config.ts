5207154736f625f9564d45281419024c501644c8
Jesse Gibson <JesseTheGibson@gmail.com>
2023-07-29 16:34:27 -0400

    Ignore test artifacts in .direnv
    Some of the test files are getting copied into that directory and
    detected by vitest.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    exclude: ['.direnv', 'node_modules', 'dist', '.git', '.cache'],
     coverage: {
       include: ['src'],
     },
